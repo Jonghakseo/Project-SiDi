cc24282c7e00f06b10f47c3da8bdc855cab1442c
JongHak Seo <53500778+Jonghakseo@users.noreply.github.com>
2020-04-05 20:54:08 +0900

    init페이지(시작페이지)레이아웃 작업중

--- app/src/main/java/com/myapp/sidi/InitSelectPage.java
+++ app/src/main/java/com/myapp/sidi/InitSelectPage.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InitSelectPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_init_select_page);
     }
     //첫 번째 주석 - 테스트
     //테스트입니다.
